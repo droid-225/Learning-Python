7a2d0e5796215143e64cf80e14a45e6da906127d
Rishit Shah <aryanrishit1@gmail.com>
2024-10-01 22:37:45 +0530

    Learned Type Casting

---
+++ typeCastin.py
@@ -0,0 +1,22 @@
+# Typecasting functions:
+# str(), int(), float(), bool()
+
+name = "Bro Code"
+age = 25
+gpa = 3.2
+is_student = True
+
+#gpa = int(gpa)
+#print(gpa)
+
+#age = float(age)
+#print(age)
+
+#age = str(age)
+#age += "1"
+#print(age)
+
+name = bool(name)
+print(name) # returns True if string is unempty, else if it is empty then it returns False
+
+#print(type(gpa)) # Type function returns variable type
